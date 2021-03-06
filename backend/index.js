ccca67eb79cb6929def805402c3daa2b5dbe47e1
hkalife <henriquekalife@gmail.com>
2021-03-06 02:33:14 -0300

    feat(crud): add user crud

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,55 @@
 // dependencies
 const express = require('express')
+const admin = require('firebase-admin')
+var bodyParser = require('body-parser')
 
-//config - express
+// config - express
 const app = express()
 
+// config - body parser
+const jsonParser = bodyParser.json()
+
+// config - firebase
+const serviceAccount = require('./serviceAccountKey.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+const db = admin.firestore()
+
 // endpoint
-app.get('/', (request, response) => {
-  response.send('Hello World!')
+app.get('/users', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  let users = []
+  db.collection('users').get().then(snapshot => {
+    snapshot.forEach((doc) => {
+      users.push(doc.data())
+    })
+    res.send(users)
+  })
 })
 
-app.get('/teste', (request, response) => {
-  let tests = {
-    id: 123456,
-    test: 'test'
-  }
-  response.send(tests)
+app.get('/user/:id', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  db.collection('users').doc(req.params.id).get().then((response) => {
+    console.log(response.data())
+    res.send(response.data())
+  });
+})
+
+app.post('/user', jsonParser, (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+
+  db.collection('users').doc().set(req.body).then(() => {
+    res.send(req.body)
+  })
+})
+
+app.put('/user/:id', jsonParser, (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  db.collection('users').doc(req.params.id).update(req.body).then(() => {
+    res.send(req.body)
+  })
 })
 
 // listen
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
